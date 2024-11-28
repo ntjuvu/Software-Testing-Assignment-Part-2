644afde518c379d23e3c83c456a2da47cdaa1343
Juuso <juuso.vuorenmaa@tuni.fi>
2024-11-28 13:45:31 +0200

    added a simple test in order to test coveralls

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-function testi(){
+import { expect } from 'chai';
+import add from './src/add.js';
 
-}
+describe('Addition', function() {
+    it('Test add two positives', function() {
+      expect(add(1,2)).to.equal(3);
+    });
+});
